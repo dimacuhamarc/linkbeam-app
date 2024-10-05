5685bac93317cadd6c068bd6a13052eba5b59ca8
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-10-05 20:18:07 +0800

    wip:cleanup starting code

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@headlessui/react';
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-full w-full">
       Linkbeam Landing Page
+      <Button className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">
+        Save changes
+      </Button>
     </div>
   );
 }
